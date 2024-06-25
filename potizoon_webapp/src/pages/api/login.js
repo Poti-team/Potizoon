f6df2339ae2170167c16a29e798e81fd950eeacf
DevCortezRN <souza.cortez@escolar.ifrn.edu.br>
2024-06-25 02:22:43 -0300

    Continuação integração google

--- potizoon_webapp/src/pages/api/login.js
+++ potizoon_webapp/src/pages/api/login.js
@@ -1,14 +1,13 @@
-import React, { useEffect } from 'react';
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import { auth } from '../../../firebaseConfig';
+const handleGoogleLogin = async () => {
+  try {
+    const provider = new GoogleAuthProvider();
+    const { user } = await signInWithPopup(auth, provider);
 
-function LoginPage() {
+    if (response.ok) {
+      const userData = await response.json();
+      console.log('Dados do usuário:', userData);
 
-  const handleGoogleLogin = async () => {
-    try {
-      const provider = new GoogleAuthProvider();
-      const { user } = await signInWithPopup(auth, provider);
-
+      // Fetch user data from `/api/getUser` using the accessToken
       const response = await fetch('/api/getUser', {
         method: 'GET',
         headers: {
@@ -17,26 +16,15 @@ function LoginPage() {
       });
 
       if (response.ok) {
-        const userData = await response.json();
-        console.log('Dados do usuário:', userData);
-        
+        const getUserData = await response.json();
+        console.log('Dados do usuário da API getUser:', getUserData);
       } else {
-        console.error('Erro ao obter dados do usuário:', response.status);
+        console.error('Erro ao obter dados do usuário da API getUser:', response.status);
       }
-    } catch (error) {
-      console.error('Erro ao fazer login com Google:', error);
+    } else {
+      console.error('Erro ao obter dados do usuário:', response.status);
     }
-  };
-
-  useEffect(() => {
-    handleGoogleLogin();
-  }, []);
-
-  return (
-    <div className="login-container">
-      <h1>Espero que o popup tenha aparecido!</h1>
-    </div>
-  );
-}
-
-export default LoginPage;
+  } catch (error) {
+    console.error('Erro ao fazer login com Google:', error);
+  }
+};
