bd9e09b6209258908a1cf2c5abcd83ea052b32b5
DevCortezRN <souza.cortez@escolar.ifrn.edu.br>
2024-07-06 01:17:44 -0300

    socorro

--- potizoon_webapp/public/images/Qual_o_sentido_disso_30_06_2024 (1)/src/appinventor/ai_ifrn_potiguaras/Qual_o_sentido_disso_30_06_2024/Leptospirose.scm
+++
@@ -1,4 +0,0 @@
-#|
-$JSON
-{"authURL":["ai2.appinventor.mit.edu"],"YaVersion":"228","Source":"Form","Properties":{"$Name":"Leptospirose","$Type":"Form","$Version":"31","ActionBar":"True","AppName":"Qual_o_sentido_disso_30_06_2024","BackgroundImage":"fundo_leptospirose.png","ShowStatusBar":"False","Title":"Leptospirose","TitleVisible":"False","Uuid":"0"}}
-|#
